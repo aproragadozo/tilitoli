9699f269b322851184dad2b4f256b0c229cc4b69
aproragadozo <aproragadozo@gmail.com>
2020-01-06 00:31:04 +0100

    in-progress fix: scrambler must update holePosition!

--- src/App.js
+++ src/App.js
@@ -29,8 +29,8 @@ class Cell extends React.Component {
     this.props.parentCallback(this.props.index);
   }
   render() {
-    var coords = this.getCoords(this.props.index, this.props.rows, this.props.cols);
-    var style = {
+    let coords = this.getCoords(this.props.index, this.props.rows, this.props.cols);
+    let style = {
       position: 'absolute',
       border: "1px solid white",
       backgroundClip: "content-box",
@@ -52,7 +52,7 @@ class Cell extends React.Component {
     <div
       ref={this.props.innerRef}
       style = { style }
-      onClick = {this.katt}>
+      onClick = {(e) => this.katt(e)}>
         {(this.props.hole === number)?"":number + 1}
     </div>
     )
@@ -65,20 +65,29 @@ class Table extends React.Component {
     // you need to duplicate the component scope
     // to later be able to call a component method
     // from within the state constructor
-    // const myself = this;
+    const myself = this;
+    const hole = 15;
     // the constant identifier for each cell
     const identifiers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-    // I should rename this "positions" or similar
-    let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+    // (I should rename this "positions" or similar)
+    // this hard-coded scramble for debugging until properly scrambled
+    let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 12, 13, 14, 11]
+    // ultimately, this is going to be numbers
+    //numbers : myself.scrambler(identifiers),
     this.state = {
+      checkSolved: this.checkSolved.bind(this),
       solved: false,
       identifiers: identifiers,
-      numbers : numbers,
+      numbers: numbers,
       // this is actually invariable,
       // so should not be doing anything in state
-      hole: 15,
+      hole: hole,
       // this is what we'll rely on to track the blank cell around the grid
-      holePosition: 15,
+      // trouble is, this "numbers" isn't "this.state.numbers",
+      // which I would need to be able to
+      // scramble cells from within the constructor
+      holePosition: numbers.indexOf(hole),
       fekete: { src: "https://i.pinimg.com/736x/25/10/1f/25101f6abb216898babcb5498197f5cb.jpg", width: 400, height: 400 }
     };
 
@@ -87,19 +96,37 @@ class Table extends React.Component {
     this.getNewLayout = this.getNewLayout.bind(this);
     this.checkSwap = this.checkSwap.bind(this);
     this.getCoords = this.getCoords.bind(this);
+    this.checkSolved = this.checkSolved.bind(this);
   }
 
-  /*
-  checkSolved = (acc, current, index) => {
-    if(current === this.state.identifiers[index]) {
-      acc = true
+// function to scramble the tiles
+  // I'll need to call it in the constructor to set the state
+  scrambler = (array) => {
+    array = array.slice(0);
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
     }
-    acc = false;
-    return acc;
+
+    return array;
   }
-  */
-  checkSolved(a1, a2) {
-    return JSON.stringify(a1)==JSON.stringify(a2);
+
+  checkSolved(arr1, arr2) {
+    for(let i = arr1.length; i--;) {
+        if(arr1[i] !== arr2[i])
+            return false;
+    }
+    return true;
   }
 
   getCoords(index, rows, cols) {
@@ -129,18 +156,13 @@ class Table extends React.Component {
     // check if clicked cell next to blank cell
     if(this.checkSwap(cellIndex, currentHole, rows, cols)) {
       let newNumbers = this.getNewLayout(numbers, cellIndex, currentHole);
-      // test
-      console.table(newNumbers);
-      console.table(this.state.identifiers);
       // update the order of cells & the position of the blank
-      this.setState({numbers: newNumbers, holePosition: cellIndex});
-      // check if solved
-      if(this.checkSolved(this.state.identifiers, this.state.numbers)){
-        console.log("barack!")
-        this.setState({solved: true});
-      }
-    }
+      this.setState((state) => {
+        return {numbers: newNumbers, holePosition: cellIndex}
+      });
   }
+}
+
   getBackgroundPos(index, img) {
       let pos;
       let coords = this.getCoords(index, this.props.rows, this.props.cols);
@@ -151,6 +173,34 @@ class Table extends React.Component {
       return pos = xPos + ' ' + yPos;
       
     }
+/*
+    scramble = (arr) => {
+      for (let elem in arr) {
+        if(elem == "up") {
+          console.log(this.state.holePosition);
+          this.swap(this.state.holePosition -4)
+        }
+        else if(elem === "left") {
+          this.swap(this.state.holePosition - 1)
+        }
+      }
+    }
+*/
+
+  static getDerivedStateFromProps(props, state) {
+    if(state.checkSolved(state.numbers, state.identifiers)) {
+      return {solved: true}
+    }
+    return null;
+  }
+
+  reRenderWhenSolved = () => {
+    console.log("yay");
+    this.setState((state) => {
+      return {numbers:this.scrambler(this.state.numbers), solved: false}
+    })
+  }
+
   render() {
     var img = new Image();
     img.src = "https://i.pinimg.com/736x/09/e6/6d/09e66dd18f0488a30753b8c20d633b16--andy-warhol-visual-arts.jpg";
@@ -160,8 +210,25 @@ class Table extends React.Component {
     lyuk.src = "https://i.pinimg.com/736x/25/10/1f/25101f6abb216898babcb5498197f5cb.jpg";
     // conditional rendering
     if(this.state.solved) {
-      return (<div>SOLVED!</div>)
-    }
+      let solvedStyle = {
+        position: 'absolute',
+        width: '50vw',
+        left: '25vw'
+      }
+      return (
+        <div
+          style={solvedStyle}>
+          <img
+            alt="yay"
+            src="https://media.giphy.com/media/yoJC2GnSClbPOkV0eA/source.gif"
+          />
+          <button
+            type="button"
+            onClick={this.reRenderWhenSolved}
+            >New Game</button>
+        </div>
+        )}
+
     return (
       <div>
         {this.state.numbers.map((number, index) => (
@@ -175,6 +242,7 @@ class Table extends React.Component {
             number = {number}
             key={this.state.identifiers[index]}
             parentCallback = {this.swap}
+            hole = {this.state.hole}
             />
         ))}
       </div >)
