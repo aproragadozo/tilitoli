16b9260823de2cc7f7f064484985ce4feece5287
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-22 22:05:32 +0000

    Deploy aproragadozo/tilitoli to github.com/aproragadozo/tilitoli.git:gh-pages

---
+++ precache-manifest.8f4cd8f502b9dfac9238ec9312adfc4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f76304f9cfc507f036b4d9cc9f88052",
+    "url": "/tilitoli/index.html"
+  },
+  {
+    "revision": "10129835509be5bbf93b",
+    "url": "/tilitoli/static/css/main.9992fd2d.chunk.css"
+  },
+  {
+    "revision": "0009fd9f19a787a8add5",
+    "url": "/tilitoli/static/js/2.80cf8676.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tilitoli/static/js/2.80cf8676.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10129835509be5bbf93b",
+    "url": "/tilitoli/static/js/main.a47c4a5c.chunk.js"
+  },
+  {
+    "revision": "823c609ce64d66209066",
+    "url": "/tilitoli/static/js/runtime-main.efcbde12.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tilitoli/static/media/logo.5d5d9eef.svg"
+  }
+]);
