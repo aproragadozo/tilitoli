65e7ed87bec9b9fd881c3c22d397a0d036b4656a
aproragadozo <aproragadozo@gmail.com>
2020-02-23 21:54:43 +0100

    Updates

---
+++ precache-manifest.3608cbdb9c1efae507070a8c7b13d784.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54e989f61f742e8fcf3b74a30c974d8",
+    "url": "/tilitoli/index.html"
+  },
+  {
+    "revision": "98621d066470394d8209",
+    "url": "/tilitoli/static/css/main.13fa4463.chunk.css"
+  },
+  {
+    "revision": "5941a2a9002d10b35943",
+    "url": "/tilitoli/static/js/2.446631be.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tilitoli/static/js/2.446631be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "98621d066470394d8209",
+    "url": "/tilitoli/static/js/main.9155d29e.chunk.js"
+  },
+  {
+    "revision": "8c0524215458bcacb469",
+    "url": "/tilitoli/static/js/runtime-main.b0a13089.js"
+  },
+  {
+    "revision": "2a79f1aa51e6cd69e3c044575e4f8fcb",
+    "url": "/tilitoli/static/media/puzzle.2a79f1aa.svg"
+  }
+]);
