d4abaf961936d871ded03178e18ab5825ae3d417
Deployment Bot (from Travis CI) <aproragadozo@gmail.com>
2020-08-04 07:12:12 +0000

    Deploy aproragadozo/tilitoli to github.com/aproragadozo/tilitoli.git:gh-pages

---
+++ precache-manifest.36c808366500747e2dbb32eca1dee8ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b707018e24cab1b500b96fbb300a688f",
+    "url": "/tilitoli/index.html"
+  },
+  {
+    "revision": "f0d4e395674d53d33dab",
+    "url": "/tilitoli/static/css/main.ea53ab9d.chunk.css"
+  },
+  {
+    "revision": "f31d7014939cedf073e7",
+    "url": "/tilitoli/static/js/2.9c4981be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tilitoli/static/js/2.9c4981be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0d4e395674d53d33dab",
+    "url": "/tilitoli/static/js/main.4a4a2d52.chunk.js"
+  },
+  {
+    "revision": "da3c3fc2df7449661143",
+    "url": "/tilitoli/static/js/runtime-main.16b44b89.js"
+  },
+  {
+    "revision": "2a79f1aa51e6cd69e3c044575e4f8fcb",
+    "url": "/tilitoli/static/media/puzzle.2a79f1aa.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tilitoli/precache-manifest.9764f0a34abe71e988579630db2a8645.js"
+  "/tilitoli/precache-manifest.36c808366500747e2dbb32eca1dee8ff.js"
 );
 
 self.addEventListener('message', (event) => {
