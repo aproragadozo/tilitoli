824639830dc71adaa9cbd845df072fead28f8ea3
aproragadozo <aproragadozo@gmail.com>
2024-09-23 11:11:25 +0200

    in-progress grid and event-handling improvements

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Cell from './Cell.js';
 */
 import Table from "./Table.js";
 import PuzzleOption from "./PuzzleOption.js";
+import Carousel from "./Carousel.js";
 
 class Tilitoli extends React.Component {
   constructor(props) {
@@ -23,6 +24,7 @@ class Tilitoli extends React.Component {
   }
 
   start = (imageFromPuzzleOptions) => {
+    console.log("alma");
     this.setState((state) => {
       return { gameOn: true, image: imageFromPuzzleOptions };
     });
@@ -40,18 +42,27 @@ class Tilitoli extends React.Component {
     this.setState((state) => {
       return { gameOn: false, images: [], ready: false };
     });
-    console.log("go");
+    //console.log("go");
     // the flickr group is https://www.flickr.com/groups/squareimg/
     let group = "17449586@N00";
     let imagesPerPage = 100;
     let noOfImages = 6;
     let randomIndices = [];
+    
+    // generating a set of non-duplicate random numbers
+    let randomSix = new Set();
+    while (randomSix.size < noOfImages) {
+      randomSix.add(Math.random());
+    };
+    //console.log(randomSix);
     let query = `https://api.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_key=${process.env.REACT_APP_FLICKR_API_KEY}&group_id=${group}&extras=url_w&per_page=${imagesPerPage}&page=1&format=json&nojsoncallback=1`;
 
-    while (noOfImages > 0) {
-      randomIndices.push(Math.floor(Math.random() * imagesPerPage));
-      noOfImages -= 1;
-    }
+  
+    // using the non-duplicate random numbers to access unique image indices
+    [...randomSix].forEach(value => {
+      randomIndices.push(Math.floor(value * imagesPerPage));
+    });
+
     // let randomIndex = Math.floor(Math.random() * imagesPerPage);
     // eslint-disable-next-line
     let images = this.getSquareImagesFromFlickr(query)
@@ -114,14 +125,7 @@ class Tilitoli extends React.Component {
       return (
         <div className="imageOptionWrapper">
           <div className="top">Dili-Toli!</div>
-          {this.state.images.map((image, index) => (
-            <PuzzleOption
-              key={index}
-              src={image}
-              className={`option${index}`}
-              click={this.start}
-            />
-          ))}
+          <Carousel images={this.state.images} onClickEvent={this.start}/>
           <div className="bottom" onClick={(e) => this.setImages(e)}>
             Get new images
           </div>
